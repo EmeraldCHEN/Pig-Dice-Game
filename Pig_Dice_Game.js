1a364ce6c11fcdc63d99bc0f46600c02f7b709e1
Your Name <you@example.com>
2019-01-08 21:25:08 +1300

    commit

---
+++ Pig_Dice_Game.js
@@ -0,0 +1,97 @@
+
+let scores, roundScore, activePlayer, dice,gamePlaying;
+//mySound = new sound("mySound.mp3"); 
+
+function gameInitialization(){   
+    scores = [0, 0];
+    roundScore = 0;
+    activePlayer = 0;
+
+    gamePlaying = true;
+
+    document.getElementById("myAudio").pause();
+    document.getElementById("myAudio").currentTime = 0;
+
+    document.querySelector('.dice').style.display = 'none'; // Get the first element in the document with '...'
+
+    document.getElementById('score_0').textContent = '0';
+    document.getElementById('score_1').textContent = '0';
+    document.getElementById('current_0').textContent = '0';
+    document.getElementById('current_1').textContent = '0';
+
+    document.getElementById('name_0').textContent = 'Player 1';
+    document.getElementById('name_1').textContent = 'Player 2';
+
+    document.querySelector('.player_0_panel').classList.remove('winner');
+    document.querySelector('.player_1_panel').classList.remove('winner');
+    document.querySelector('.player_0_panel').classList.remove('active');
+    document.querySelector('.player_1_panel').classList.remove('active');
+    document.querySelector('.player_0_panel').classList.add('active');
+}
+gameInitialization();
+
+document.querySelector('.btn_roll').addEventListener('click', function(){
+    if(gamePlaying){
+
+        // 1. Random umber
+        let dice = Math.floor(Math.random() * 6) + 1;
+            
+        // 2. Display the result
+        let diceDOM = document.querySelector('.dice');
+        diceDOM.style.display = 'block'; 
+        diceDOM.src = 'image/dice-' + dice + '.png';
+
+        // 3. Update the round score if the rolled number was NOT a one
+        if(dice !== 1){
+            // Add round score
+            roundScore += dice;
+            document.getElementById('current_' + activePlayer).textContent = roundScore;
+        }else{
+            // Next player
+            nextPlayer();           
+        }
+    }   
+});
+
+document.querySelector('.btn_hold').addEventListener('click', function(){
+    if(gamePlaying){
+        // Add CURRENT score to GLOBAL score
+        scores[activePlayer] += roundScore;
+
+        // Update the UI
+        document.getElementById('score_' + activePlayer).textContent = scores[activePlayer];
+
+        // Check if player won the game
+        if(scores[activePlayer] >= 10){
+            document.getElementById('name_' + activePlayer).textContent = 'Winner!';
+            document.querySelector('.dice').style.display = 'none';
+            document.querySelector('.player_' + activePlayer + '_panel').classList.add('winner');
+            document.querySelector('.player_' + activePlayer + '_panel').classList.remove('active');
+            gamePlaying = false;
+           // mySound.play(); // Musice would be played when one player becomes a winner
+           document.getElementById("myAudio").play();
+        }else{
+            nextPlayer();
+        }
+    }
+});
+
+function nextPlayer(){
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+    document.getElementById('current_0').textContent = '0';
+    document.getElementById('current_1').textContent = '0';
+
+    document.querySelector('.player_0_panel').classList.toggle('active');
+    document.querySelector('.player_1_panel').classList.toggle('active');
+
+    document.querySelector('.dice').style.display = 'none';  
+}
+
+
+//document.querySelector('.btn_new').addEventListener('click', gameInitialization);
+
+
+//document.querySelector('#current_' + activePlayer).textContent = dice;
+//document.querySelector('#current_' + activePlayer).innerHTML = '<em>'+ dice +'</em>'; // emphasized text
+//let x = document.querySelector('#score_0').textContent;
--- README.md
+++ README.md
@@ -3,7 +3,16 @@
 
 This program is mostly the project from the vedio course named The Complete JavaScript Course 2018: Build Real Projects! @Udemy
 
-It is a simple dice game with just a single die. You win by being the first player to score 100 or more points.
+It is a simple dice game with just a single die. 
+
+GAME RULES:
+- The game has 2 players, playing in rounds
+- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
+- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The first player to reach 100 points on GLOBAL score wins the game
+
+You win by being the first player to score 100 or more points.
 
 Instructor: Jonas Schmedtmann
 
